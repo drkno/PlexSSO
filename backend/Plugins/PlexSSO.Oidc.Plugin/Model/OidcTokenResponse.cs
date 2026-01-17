89138e4b547c5576a9c10664ba15ebd32e3e8b2c
Matthew Knox <matthew@makereti.co.nz>
2026-01-17 19:44:31 +1100

    Use correct oidc json names in response

--- backend/Plugins/PlexSSO.Oidc.Plugin/Model/OidcTokenResponse.cs
+++ backend/Plugins/PlexSSO.Oidc.Plugin/Model/OidcTokenResponse.cs
@@ -1,9 +1,18 @@
 namespace PlexSSO.Oidc.Plugin.Model;
 
+using System.Text.Json.Serialization;
+
 public class OidcTokenResponse
 {
+    [JsonPropertyName("access_token")]
     public string AccessToken { get; set; }
+
+    [JsonPropertyName("id_token")]
     public string IdToken { get; set; }
+
+    [JsonPropertyName("expires_in")]
     public int? ExpiresIn { get; set; }
+
+    [JsonPropertyName("token_type")]
     public string TokenType { get; set; } = "Bearer";
 }
